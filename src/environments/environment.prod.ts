8b6121d33e1e89a45bf1658bd70eed36a5a4f0d9
Diego Landa <dlandat95@gmail.com>
2026-07-02 02:01:43 -0500

    variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,10 +7,10 @@ export const environment = {
   apiUrl: 'https://emplix-api-prod.azurewebsites.net/api',
   defaultTenant: 'demo',
   // URL de tu frontend en Vercel (o tu dominio custom)
-  redirectUri: 'https://proud-forest-0baae950f.7.azurestaticapps.net',
+  redirectUri: 'https://ambitious-glacier-018d9ed0f.7.azurestaticapps.net/',
   azure: {
     // App Registration de PRODUCCIÓN en portal.azure.com (diferente al de DEV)
     clientId: '40fdfa96-fd34-4c7e-82b0-905097c87714',
-    popupRedirectUri: 'https://proud-forest-0baae950f.7.azurestaticapps.net/auth-redirect.html',
+    popupRedirectUri: 'https://ambitious-glacier-018d9ed0f.7.azurestaticapps.net/auth-redirect.html',
   },
 };
